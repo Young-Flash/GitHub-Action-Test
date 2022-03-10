dc10f4f114d708cfe1a4989065b50c38e68aa80c
Flash <2020022950@m.scnu.edu.cn>
2022-03-10 10:26:15 +0800

    add files

---
+++ src/main/GAT.java
@@ -0,0 +1,10 @@
+package main;
+
+public class GAT {
+    public static void work() {
+        System.out.println("This is a GitHub Action Test.");
+    }
+    public static void main(String[] args) {
+        work();
+    }
+}
