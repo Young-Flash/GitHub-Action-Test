2ebbe4d5761a1ab9a6db791933bfff3119c89ba5
Young-Flash <71162630+Young-Flash@users.noreply.github.com>
2022-03-11 17:04:09 +0800

    Update GAT.java

--- src/main/java/indi/flash/impl/GAT.java
+++ src/main/java/indi/flash/impl/GAT.java
@@ -1,5 +1,9 @@
 package indi.flash.impl;
 public class GAT {
+    /**
+     * This is javadoc
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("GAT");
     }
