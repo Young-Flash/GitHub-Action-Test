5577d361d7ab6d9896c29c717aab4785d857acf5
Flash <2020022950@m.scnu.edu.cn>
2022-03-13 11:02:33 +0800

    4.0.0

--- src/main/java/indi/flash/impl/GAT.java
+++ src/main/java/indi/flash/impl/GAT.java
@@ -1,6 +1,13 @@
 package indi.flash.impl;
 
+import com.vesoft.nebula.client.graph.net.NebulaPool;
+
 public class GAT {
+    /**
+     * This is javadoc
+     * @param args
+     */
+    private NebulaPool pool;
     public static void main(String[] args) {
         System.out.println("GAT");
     }
